312902caef5eb231673b5a0e2cb12b1764e3efe6
kelly <kelly.n.waite@gmail.com>
2017-03-13 12:48:04 -0600

    Finish exercise 2

--- js/modal.js
+++ js/modal.js
@@ -9,10 +9,45 @@ cancelButton.addEventListener('click', close);
 // Get a list of tabbable elements here:
 // https://github.com/jkup/focusable
 
+
 function open() {
   // Show the modal and overlay
+  let previouslyFocusedElement = document.activeElement;
+
+  let tabbableElements = modal.querySelectorAll('a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex="0"], [contenteditable]')
+
+  // tabbableElements = Array.prototype.slice.call(tabbableElements);
+
+  const firstItem = tabbableElements[0];
+  const lastItem = tabbableElements[tabbableElements.length - 1]
+
+  modal.addEventListener('keydown', trap)
+
   modal.style.display = 'block';
   modalOverlay.style.display = 'block';
+
+  function trap(event) {
+    if(event.keyCode === 9) {
+      // Shift is held down
+      if(event.shiftKey) {
+        // backwards
+        if (document.activeElement === firstItem) {
+          event.preventDefault();
+          lastItem.focus()
+        }
+      }
+      else {
+        if (document.activeElement === lastItem) {
+          event.preventDefault();
+          firstItem.focus();
+        }
+      }
+    }
+      // Shift is NOT held down
+    else if (event.keyCode === 27) {
+      close()
+    }
+  }
 }
 
 function close() {
